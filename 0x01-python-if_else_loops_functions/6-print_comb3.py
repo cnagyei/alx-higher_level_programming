74c02ac36d7d71af255085d3f6c11ad099a66db1
cnagyei <cnagyei15@gmail.com>
2024-05-16 14:36:27 -0700

    print combo complex

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 for num1 in range(0, 10):
-    for num2 in range (1, 10):
+    for num2 in range(1, 10):
         if (num1 == 8) and (num2 == 9):
             print("{}{}".format(num1, num2))
         else:
             if int(f"{num1}{num2}") < int(f"{num2}{num1}"):
                 print("{}{}".format(num1, num2), end=", ")
-
