7c12e5eaf1d4c3df985258e3b16be85d5cd75a19
cnagyei <cnagyei15@gmail.com>
2024-05-16 18:04:44 -0700

    convert to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,9 @@
 def uppercase(str):
     """Prints a string in uppercase followed by a new line"""
 
-    for c in range(len(str)):
-        ascii_pt = ord(str[c])
+    for c in str:
+        ascii_pt = ord(c)
         if ascii_pt >= 97 and ascii_pt <= 122:
-            print("{}".format(chr(ascii_pt - 32)), end="")
-        else:
-            print("{}".format(str[c]), end="")
+            c = chr(ascii_pt - 32)
+        print("{}".format(c), end="")
+    print("")
