e8d07f3e963aeb8df4a68eab89584c7bb49e7ecb
cnagyei <cnagyei15@gmail.com>
2024-05-18 21:02:47 -0700

    parse args to int and add them

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         print("{}".format(argv[1]))
     elif len(argv) > 2:
         sum = 0
-        for i in range (1, len(argv)):
+        for i in range(1, len(argv)):
             sum += int(argv[i])
         print("{}".format(sum))
