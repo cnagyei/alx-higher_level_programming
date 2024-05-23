7cd2eacce512a418ae06429e178b860cea7eb2a3
cnagyei <cnagyei15@gmail.com>
2024-05-22 19:55:04 -0700

    Full Join

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
 -- List all genres of a show
-SELECT tv_genres.name
+SELECT tv_genres.name AS name
   FROM tv_shows
   JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
   JOIN tv_genres
     ON tv_show_genres.genre_id = tv_genres.id
   WHERE tv_shows.id = 8
-  ORDER BY tv_genres.name;
+  ORDER BY name;
