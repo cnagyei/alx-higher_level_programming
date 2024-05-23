55663b26b71f7c4693faea0caf39d1b72dc117ac
cnagyei <cnagyei15@gmail.com>
2024-05-22 17:37:28 -0700

    create new database and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2;
 
--- Create user
+-- Create user and grant permission
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
