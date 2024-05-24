e43a2a7cf4494b66fd731a4e723347b1181c51a4
cnagyei <cnagyei15@gmail.com>
2024-05-24 08:46:20 -0700

    retrieve element at index from list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """Retrieve an element from a list like in C
+
+    Arguments:
+    my_list -- the list
+    idx -- the index of element to retrieve
+    """
+    if idx > (len(my_list) - 1) or idx < 0:
+        return (None)
+    return (my_list[idx])
+
+if __name__ == "__main__":
+    import sys
