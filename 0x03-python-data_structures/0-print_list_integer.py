c851a5d0c8ab654993ced044a6e63e9a09766b30
cnagyei <cnagyei15@gmail.com>
2024-05-24 08:31:01 -0700

    print integer list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Print all integers of a list.
+
+    Arguments:
+    my_list -- the list
+    """
+    for i in my_list:
+        print("{}".format(i))
+
+if __name__ == "__main__":
+    import sys
