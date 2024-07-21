fa0d7d1e3b7154c6ce225eb1b898f16ddb667f57
cnagyei <cnagyei15@gmail.com>
2024-07-21 15:54:41 +0000

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers.
+
+    Arguments:
+    matrix -- Matrix of integers
+    """
+    for row in matrix:
+        for element in row:
+            print(element, end=" ")
+        print()
+
+if __name__ == "__main__":
+    import sys
