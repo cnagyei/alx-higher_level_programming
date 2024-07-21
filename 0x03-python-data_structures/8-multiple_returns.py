ceaf328e911bf28f72460c5addde5dca0a6c0c54
cnagyei <cnagyei15@gmail.com>
2024-07-21 23:19:55 +0000

    Return tuple with length of string and first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """Return tuple with length of string and first character.
+
+    Arguments:
+    sentence -- string
+    """
+    strlen = len(sentence)
+    if strlen == 0:
+        return ((0, None))
+        exit (1)
+
+    first_char = sentence[0]
+
+    return ((strlen, first_char))
+
+
+if __name__ == "__main__":
+    import sys
