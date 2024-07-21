4447be94ed8272284258c41979a73db94003150d
cnagyei <cnagyei15@gmail.com>
2024-07-21 16:34:03 +0000

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,8 +6,8 @@ def print_matrix_integer(matrix=[[]]):
     matrix -- Matrix of integers
     """
     for row in matrix:
-        for element in row:
-            print("{}".format(element), end=" " if element != row[-1] else "")
+        for col in row:
+            print("{}".format(col), end=" " if col != row[-1] else "")
         print()
 
 
