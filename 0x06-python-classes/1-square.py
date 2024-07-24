24cc4bfaac8350bc760bd0fc7adcf54b84f5d2ea
cnagyei <cnagyei15@gmail.com>
2024-07-24 14:49:30 +0000

    class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a class square"""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initalizing square.
+        Args:
+            size: The size of the square
+        """
+        self.__size = size
